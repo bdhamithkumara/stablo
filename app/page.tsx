9d55cbe2e7c93c2329c6f0f8964678d640d545ca
bdhamithkumara <damithkumararoxx55@gmail.com>
2025-02-24 23:24:17 +0530

    until 2 24

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+    return (
+      <div>
+        <h1>Welcome to My Website</h1>
+        <p>This is the landing page.</p>
+      </div>
+    );
+  }
+  
---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { type NextRequest } from 'next/server'
+import updateSession from '@/utils/supabase/middleware'
+
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
+}
+
+export const config = {
+  matcher: [
+    // Match all request paths except for:
+    // - _next/static (static files)
+    // - _next/image (image optimization files)
+    // - favicon.ico (favicon file)
+    '/dashboard/:path*'
+  ],
+};
